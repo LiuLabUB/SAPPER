195b41411ce6e7c7356cf2b1d5af42918c21010f
Tao Liu <vladimir.liu@gmail.com>
2015-08-13 18:20:56 -0400

    auto detect fermi;mkdtemp to create temp dir.

--- SNV-calling-AS-binding-prediction/src/main.cpp
+++ SNV-calling-AS-binding-prediction/src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,59 +10,83 @@
 using namespace std;
 
 
+std::string lookupFermi()
+{
+  std::vector<std::string> path_array;
+
+  char *dup = strdup(getenv("PATH"));
+  char *s = dup;
+  char *p = NULL;
+
+  do {
+    p = strchr(s, ':');
+    if (p != NULL) {
+      p[0] = 0;
+    }
+    path_array.push_back(s);
+    s = p+1;
+  } while (p != NULL);
+
+  for (int i = 0; i < path_array.size(); ++i)
+    {
+      std::string temp = path_array[i];
+
+      temp = temp + "/" + "fermi";
+      // let execv determine if it is executable
+      // or you can do that here if required
+      if ( access(temp.c_str(), F_OK) == 0)
+	{
+	  cout << "Executable fermi binary file found at " << temp << "\n";
+	  return temp;
+	}
+    }
+
+  cout << "Executable fermi binary file can't be found in PATH. Please make sure fermi is installed in the system!\n";
+  return("");
+}
 
 int main(int argc,char *argv[])
 {
-	if(argc<8)
-	{
-		cout<<"\nProgram: SNVAS (call SNV and allele-specific events from ChIP-seq data)\n";
-		cout<<"Version: 0.1\n";
-		cout<<"Usage: SNVAS <peaks.bed> <peaks.bam> <controlpeaks.bam> {PE,SE} <output.vcf> <fermi> <tmp>\n\n";
-		cout<<"Options: <peaks.bed>            sorted bed file of peak regions\n";
-		cout<<"         <peaks.bam>            sorted bam file of peak regions\n";
-		cout<<"         <controlpeaks.bam>     sorted control bam file of peak regions\n";
-		cout<<"         {PE,SE}                format of bam file, PE (paired-end) or SE (single-end)\n";
-		cout<<"         <output.vcf>           output vcf file\n";
-		cout<<"         <fermi>                path of fermi executable file\n";
-		cout<<"         <tmp>                  temparary file folder\n\n";
-	}
-	else
-	{
-		const string Peakbedfile(argv[1]);
-		const string Bamfile(argv[2]);
-		const string InputBamfile(argv[3]);
-		const string PEorSE(argv[4]);
-		const string OutputVcffile(argv[5]);
-		const string fermi_location(argv[6]);
-		const string tmpfilefolder(argv[7]);
 
-		if(PEorSE!="PE" && PEorSE!="SE") {cout<<"wrong format {PE,SE}"<<endl;exit(0);}
+  if(argc<6)
+    {
+      cout<<"\nProgram: SNVAS (call SNV and allele-specific events from ChIP-seq data)\n";
+      cout<<"Version: 0.1\n";
+      cout<<"Usage: SNVAS <peaks.bed> <peaks.bam> <controlpeaks.bam> {PE,SE} <output.vcf> <fermi> <tmp>\n\n";
+      cout<<"Options: <peaks.bed>            sorted bed file of peak regions\n";
+      cout<<"         <peaks.bam>            sorted bam file of peak regions\n";
+      cout<<"         <controlpeaks.bam>     sorted control bam file of peak regions\n";
+      cout<<"         {PE,SE}                format of bam file, PE (paired-end) or SE (single-end)\n";
+      cout<<"         <output.vcf>           output vcf file\n";
+    }
+  else
+    {
+      const string Peakbedfile(argv[1]);
+      const string Bamfile(argv[2]);
+      const string InputBamfile(argv[3]);
+      const string PEorSE(argv[4]);
+      const string OutputVcffile(argv[5]);
+      const string fermi_location = lookupFermi();
 
-		/*string sbuf,fermi_location,tmpfilefolder;
-		ifstream is("config.txt");
-		if(!is) {cout<<"can not open bedtools location file: config.txt"<<endl;exit(0);}
-		is>>sbuf>>fermi_location;
-		if(sbuf!="fermi:") {cout<<"wrong format of config.txt"<<endl;exit(0);}
-		if(fermi_location.empty()) {cout<<"wrong format of config.txt"<<endl;exit(0);}
-		sbuf.clear();
-		is>>sbuf>>tmpfilefolder;
-		if(sbuf!="tmp_filefolder:") {cout<<"wrong format of config.txt"<<endl;exit(0);}
-		if(tmpfilefolder.empty()) {cout<<"wrong format of config.txt"<<endl;exit(0);}
-		is.close();*/
+      //if (fermi_location == "")
+      //return(0);
 
-		mkdir(tmpfilefolder.c_str(), 0755);
+      // make temp dir 
+      const string tmpfilefolder( mkdtemp( "/tmp/SNVAS.XXXXXX" ) );
 
-		//read peak region bed file
-		vector<BedRegion> peakbedregion_set;//sorted by bam header chr order
-		ReadPeakBedFile(Peakbedfile,Bamfile,peakbedregion_set);
-cout<<"finish read bed"<<endl;
-		//read Input bam file
-		vector<vector<BamInfor> > AllPeakInputBamInfor;
-		ReadInputBamfile(peakbedregion_set,InputBamfile,AllPeakInputBamInfor);
-cout<<"finish read input bam"<<endl;
-		//read bam file and calculate
-		OutputVcfResultHasInput_header(OutputVcffile);
-		ReadBamfile(PEorSE,peakbedregion_set,Bamfile,AllPeakInputBamInfor,fermi_location,tmpfilefolder,OutputVcffile);
-cout<<"finishall"<<endl;
-	}
+      if(PEorSE!="PE" && PEorSE!="SE") {cout<<"wrong format {PE,SE}"<<endl;exit(0);}
+      
+      //read peak region bed file
+      vector<BedRegion> peakbedregion_set;//sorted by bam header chr order
+      ReadPeakBedFile(Peakbedfile,Bamfile,peakbedregion_set);
+      cout<<"finish read bed"<<endl;
+      //read Input bam file
+      vector<vector<BamInfor> > AllPeakInputBamInfor;
+      ReadInputBamfile(peakbedregion_set,InputBamfile,AllPeakInputBamInfor);
+      cout<<"finish read input bam"<<endl;
+      //read bam file and calculate
+      OutputVcfResultHasInput_header(OutputVcffile);
+      ReadBamfile(PEorSE,peakbedregion_set,Bamfile,AllPeakInputBamInfor,fermi_location,tmpfilefolder,OutputVcffile);
+      cout<<"finish all"<<endl;
+    }
 }
