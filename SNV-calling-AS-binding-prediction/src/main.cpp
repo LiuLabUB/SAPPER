bfa91805a420a43c4fe59d5769d38a9084fe2317
Tao Liu <tliu4@u10n28.ccr.buffalo.edu>
2015-08-13 23:03:02 -0400

    Now temporary dir will be created using template 'SNVAS.XXXXXX' at the current working dir. When main program quit, it will be removed.

--- SNV-calling-AS-binding-prediction/src/main.cpp
+++ SNV-calling-AS-binding-prediction/src/main.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 
 
-std::string lookupFermi()
+string lookupFermi()
 {
-  std::vector<std::string> path_array;
+  vector<string> path_array;
 
   char *dup = strdup(getenv("PATH"));
   char *s = dup;
@@ -29,7 +29,7 @@ std::string lookupFermi()
 
   for (int i = 0; i < path_array.size(); ++i)
     {
-      std::string temp = path_array[i];
+      string temp = path_array[i];
 
       temp = temp + "/" + "fermi";
       // let execv determine if it is executable
@@ -47,8 +47,7 @@ std::string lookupFermi()
 
 int main(int argc,char *argv[])
 {
-
-  if(argc<6)
+  if ( argc < 6 )
     {
       cout<<"\nProgram: SNVAS (call SNV and allele-specific events from ChIP-seq data)\n";
       cout<<"Version: 0.1\n";
@@ -58,35 +57,40 @@ int main(int argc,char *argv[])
       cout<<"         <controlpeaks.bam>     sorted control bam file of peak regions\n";
       cout<<"         {PE,SE}                format of bam file, PE (paired-end) or SE (single-end)\n";
       cout<<"         <output.vcf>           output vcf file\n";
+      return(1);
     }
-  else
-    {
-      const string Peakbedfile(argv[1]);
-      const string Bamfile(argv[2]);
-      const string InputBamfile(argv[3]);
-      const string PEorSE(argv[4]);
-      const string OutputVcffile(argv[5]);
-      const string fermi_location = lookupFermi();
+  
+  const string Peakbedfile(argv[1]);
+  const string Bamfile(argv[2]);
+  const string InputBamfile(argv[3]);
+  const string PEorSE(argv[4]);
+  const string OutputVcffile(argv[5]);
+  const string fermi_location(lookupFermi());
 
-      //if (fermi_location == "")
-      //return(0);
+  if (fermi_location == "")
+    return(0);
 
-      // make temp dir 
-      const string tmpfilefolder( mkdtemp( "/tmp/SNVAS.XXXXXX" ) );
+  // make temp dir 
+  char ctemplate[] = "SNVAS.XXXXXX";
+  char * ctmpfilefolder = mkdtemp( ctemplate );
+  const string tmpfilefolder( ctmpfilefolder );
 
-      if(PEorSE!="PE" && PEorSE!="SE") {cout<<"wrong format {PE,SE}"<<endl;exit(0);}
+  // check PE/SE mode
+  if(PEorSE!="PE" && PEorSE!="SE") {cout<<"wrong mode! Choose from {PE,SE}"<<endl;exit(0);}
       
-      //read peak region bed file
-      vector<BedRegion> peakbedregion_set;//sorted by bam header chr order
-      ReadPeakBedFile(Peakbedfile,Bamfile,peakbedregion_set);
-      cout<<"finish read bed"<<endl;
-      //read Input bam file
-      vector<vector<BamInfor> > AllPeakInputBamInfor;
-      ReadInputBamfile(peakbedregion_set,InputBamfile,AllPeakInputBamInfor);
-      cout<<"finish read input bam"<<endl;
-      //read bam file and calculate
-      OutputVcfResultHasInput_header(OutputVcffile);
-      ReadBamfile(PEorSE,peakbedregion_set,Bamfile,AllPeakInputBamInfor,fermi_location,tmpfilefolder,OutputVcffile);
-      cout<<"finish all"<<endl;
-    }
+  //read peak region bed file
+  vector<BedRegion> peakbedregion_set;//sorted by bam header chr order
+  ReadPeakBedFile(Peakbedfile,Bamfile,peakbedregion_set);
+  cout<<"finish read bed"<<endl;
+  //read Input bam file
+  vector<vector<BamInfor> > AllPeakInputBamInfor;
+  ReadInputBamfile(peakbedregion_set,InputBamfile,AllPeakInputBamInfor);
+  cout<<"finish read input bam"<<endl;
+  //read bam file and calculate
+  OutputVcfResultHasInput_header(OutputVcffile);
+  ReadBamfile(PEorSE,peakbedregion_set,Bamfile,AllPeakInputBamInfor,fermi_location,tmpfilefolder,OutputVcffile);
+  cout<<"finish all"<<endl;
+
+  // remove temp dir
+  remove( ctmpfilefolder );
 }
